86a1607524a6c01db4d4c954faa48ed2eabe3c08
Jc9653 <jose.carmona9653@gmail.com>
2024-04-18 23:25:46 -0500

    things

--- connector_test.py
+++ connector_test.py
@@ -1,10 +1,19 @@
 import mysql.connector
 
-connect = mysql.connector.connect(
+connection = mysql.connector.connect(
     user = 'root',
     database = 'example',
     password = 'Cv@#nmDg%!8'
 )
 
 cursor = connection.cursor()
+testQuery = ("SELECT * FROM bankaccounts")
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+
+cursor.close()
+connection.close()
 
