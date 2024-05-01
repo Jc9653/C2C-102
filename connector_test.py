dcfab9a36bdb5b6c47e117c36f6a933c6307a45d
Jc9653 <jose.carmona9653@gmail.com>
2024-04-30 23:32:55 -0500

    Lots of Stuff

--- connector_test.py
+++ connector_test.py
@@ -7,8 +7,16 @@ connection = mysql.connector.connect(
 )
 
 cursor = connection.cursor()
-testQuery = ("SELECT * FROM bankaccounts")
-cursor.execute(testQuery)
+#addData = ("INSERT INTO bankaccounts(UserName, Password, Balance)VALUES('bimmy', '123Password', 520.3)")
+
+#cursor.execute(addData)
+
+
+#connection.commit()
+cursor.execute("UPDATE bankaccounts SET balance = (balance + 50) WHERE id = 6")
+#cursor.execute("SELECT * FROM bankaccounts")
+cursor.execute("SELECT balance FROM bankaccounts WHERE id = 6") 
+
 
 for item in cursor:
     print(item)
@@ -17,3 +25,35 @@ for item in cursor:
 cursor.close()
 connection.close()
 
+
+
+
+
+#adds an account to data base
+def addAccount(userName, password, balance):
+    addQuery = ("INSERT INTO bankaccounts (UserName, Password, Balance) VALUES (" + userName + ", " + password + ", " + str(balance) + ")")
+    cursor.execute(addQuery) 
+    connection.commit()
+#deletes account from database
+def deleteAccount(id):
+    deleteQuery = ("DELETE FROM bankaccounts WHERE ID = " + str(id))
+    cursor.execute(deleteQuery)
+    print("Account Deleted")
+
+#Adds money to account balance
+def Deposit(id, amount):
+    cursor.execute("UPDATE bankaccounts SET balance = (balance +" + str(amount) + ") WHERE id = " + str(id))
+
+#widthdraws money from account balance
+def Withdraw(id, amount):
+    cursor.execute("UPDATE bankaccounts SET balance = (balance -" + str(amount) + ") WHERE id = " + str(id))
+
+#prints the balance
+def ViewBalance(id):
+    cursor.execute("SELCET balance FROM bankaccounts WHERE id = " + str(id))
+    for item in cursor:
+        print(item)
+
+def GetIdNum(username):
+    return cursor.execute("SELECT id FROM bankaccounts WHERE UserName = " + username)
+    
--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-print("Welcome User")
-print("What Would You Like to Do:")
-print("-------------------------")
-print("Option 1: Create Account")
-print("Option 2: Edit Account")
-print("Option 3: Delete Account")
-print("Option 4: Sign In")
-print("-------------------------")
+import connector_test
+run = True
+signedIn = False
+UserChoice = ""
+#menu for account page
+def accountPage(username, password):
+    while signedIn:
+        print("-------------------------")
+        print("1 --View Balance")
+        print("2 --Deposit")
+        print("3 --Widthdraw")
+        print("4 --Sign Out")
+        print("-------------------------")
+        print("")
+
+#While loop used to keep code running
+while run:
+    #menu Screen
+    print("Welcome User")
+    print("What Would You Like to Do:")
+    print("-------------------------")
+    print("1 --Create Account")
+    print("2 --Sign In")
+    print("3 --Exit")
+    print("-------------------------")
+    print("")
+
+    #code for Options the User can make
+    UserChoice = input("Select Option(1-3): ")
+    if(UserChoice == "1"):
+        username = input("Enter A UserName: ")
+        password = input("Enter A password: ")
+        balance = input("Enter Initial Balance: ")
+        print("You have Created an account")
+        print(connector_test.GetIdNum(username))
+        signedIn = True
+    elif(UserChoice == "2"):
+        username = input("Enter A UserName: ")
+        password = input("Enter A Password: ")
+        signedIn = True
+        print("You are now Sign In")
+    elif(UserChoice == "3"):
+        run = False
+
+
